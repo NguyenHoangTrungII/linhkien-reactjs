12a7dc57b9b07b639bf600ee019c6c16a14a0ec9
NguyenhoangtrungII <nguyenhoangtrunghs@gmail.com>
2023-08-31 19:52:45 +0700

    commit page Cart

--- src/component/CartDetail/CartDetail.js
+++ src/component/CartDetail/CartDetail.js
@@ -2,14 +2,18 @@ import classNames from 'classnames/bind';
 
 import styles from './CartDetail.module.scss';
 import Table from './Component/Table';
+import Button from '../Button/Button';
+import InputText from '../Input/Input';
+
+// import { Table } from 'antd';
 
 const cx = classNames.bind(styles);
 
 function CartDetail() {
     const rowClass = cx('row', 'cart-container');
     const col1Class = cx('col-12', 'productdetail-wrapper');
-    const col2Class = cx('col-xl-6', 'col-lg-6', 'col-md-6', 'detail-wrapper');
-    const col3Class = cx('col-xl-6', 'col-lg-6', 'col-md-6', 'billing-wrapper');
+    const col2Class = cx('col-12', 'action-wrapper');
+    const col3Class = cx('col-12', 'col-md-12', 'coupon-wrapper');
 
     const columns = ['Product', 'Price', 'Quantity', 'Subtotal'];
     const data = [
@@ -32,6 +36,20 @@ function CartDetail() {
             price: '60000',
             name: 'CPU intel core i9 12900',
         },
+
+        {
+            _id: 4,
+            img: require('~/public/uploads/products/CPU/Thumbnail/Product_Thumbnail_570x470_cpu-intel-core-i9-12900 (1).png'),
+            price: '60000',
+            name: 'CPU intel core i9 12900',
+        },
+
+        // {
+        //     _id: 5,
+        //     img: require('~/public/uploads/products/CPU/Thumbnail/Product_Thumbnail_570x470_cpu-intel-core-i9-12900 (1).png'),
+        //     price: '60000',
+        //     name: 'CPU intel core i9 12900',
+        // },
     ];
 
     return (
@@ -39,8 +57,45 @@ function CartDetail() {
             <div className={col1Class}>
                 <Table columns={columns} data={data} />
             </div>
-            <div className={col2Class}></div>
-            <div className={col3Class}></div>
+            <div className={col2Class}>
+                <Button outline>Return To Shop</Button>
+                <Button outline>Update Cart</Button>
+            </div>
+            <div className={col3Class}>
+                <div className={cx('coupon-apply')}>
+                    <InputText outline placeholder="Coupon Code" className={cx('coupon-input')} />
+                    <Button primary className={cx('coupon-btn')}>
+                        Apply Coupon
+                    </Button>
+                </div>
+
+                <div className={cx('cart-total')}>
+                    {/* total details */}
+                    <div className={cx('cart-total-inner')}>
+                        <span className={cx('title')}>Cart Total</span>
+                        <div>
+                            <div className={cx('payment')}>
+                                <span className={cx('text')}>Subtotal:</span>
+                                <span className={cx('price')}>{200000}</span>
+                            </div>
+
+                            <div className={cx('payment')}>
+                                <span className={cx('text')}>Ship:</span>
+                                <span className={cx('price')}>{200000}</span>
+                            </div>
+
+                            <div className={cx('payment')}>
+                                <span className={cx('text')}>Total:</span>
+                                <span className={cx('price')}>{200000}</span>
+                            </div>
+                        </div>
+
+                        <div>
+                            <Button primary>Procees to checkout</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/component/CartDetail/Component/Table/Table.js
+++ src/component/CartDetail/Component/Table/Table.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import classNames from 'classnames/bind';
+// import { InputNumber } from 'ant';
 
 import formatCurrency from '~/helpers/currencyFormatter';
 import styles from './Table.module.scss';
+import { InputNumber } from 'antd';
 
 const cx = classNames.bind(styles);
 
@@ -20,7 +22,7 @@ function Table(props) {
             </thead>
             <tbody>
                 {data.map((item, index) => (
-                    <tr key={index}>
+                    <tr key={index} className={cx('cart-row')}>
                         <td className={cx('product')}>
                             <img src={item.img} />
                             <p>{item.name}</p>
@@ -28,12 +30,39 @@ function Table(props) {
                         <td>
                             <span>{formatCurrency(item.price)}</span>
                         </td>
-                        <td></td>
-                        <td></td>
+                        <td>
+                            <InputNumber size={'middle'} className={cx('input-number')} min={0} defaultValue={1} />
+                        </td>
+                        <td>a</td>
                     </tr>
                 ))}
             </tbody>
         </table>
+
+        // <table>
+        //     <tr>
+        //         {columns.map((column, index) => (
+        //             <th key={index} style={{ width: '25%' }}>
+        //                 {column}
+        //             </th>
+        //         ))}
+        //     </tr>
+        //     {data.map((item, index) => (
+        //         <tr key={index} className={cx('cart-row')}>
+        //             <td className={cx('product')}>
+        //                 <img src={item.img} />
+        //                 <p>{item.name}</p>
+        //             </td>
+        //             <td>
+        //                 <span>{formatCurrency(item.price)}</span>
+        //             </td>
+        //             <td>
+        //                 <InputNumber size={'middle'} className={cx('input-number')} min={0} />
+        //             </td>
+        //             <td>a</td>
+        //         </tr>
+        //     ))}
+        // </table>
     );
 }
 
